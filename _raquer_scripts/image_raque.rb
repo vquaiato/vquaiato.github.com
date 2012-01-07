c519501170831bd57b586f8f506728a10da7d78b
vquaiato <vinicius.quaiato@gmail.com>
2012-01-07 18:56:42 -0200

    some images

--- _raquer_scripts/image_raque.rb
+++ _raquer_scripts/image_raque.rb
@@ -1,11 +1,11 @@
-Dir.foreach("../_posts/") do |file|
+Dir.foreach("../_posts_wordpress/") do |file|
 	p "started #{file}"
-
 	begin
-		content  = File.read("../_posts/#{file}")
+		content  = File.read("../_posts_wordpress/#{file}")
 		images_found = content.scan /http\S*\.[jpg|gif|png]+/i #/http\S*\.jpg|gif|png/i
 
 		images_found.each do |img|
+			new_file = File.open("imagens", "a+"){|f| f.puts img}
 			system "curl #{img} -O"
 			p "image saved: #{img}"
 		end
